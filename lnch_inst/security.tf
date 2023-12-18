d55fb6dc5f282435eb084e0d428362c1927a027e
cdrAbhi <149069370+cdrAbhi@users.noreply.github.com>
2023-12-18 18:35:12 +0530

    Create security.tf

---
+++ lnch_inst/security.tf
@@ -0,0 +1,32 @@
+
+# use to create security_group
+
+resource "aws_security_group" "aws_sg" {
+  name        = "aws_sg"
+  description = "By_using_terrafrom"
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "tr_sg"
+  }
+
+
+}
+
+
