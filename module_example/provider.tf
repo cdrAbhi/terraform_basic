62a099c82cb6f959431a7cb5b860b8566ea94dd5
cdrAbhi <149069370+cdrAbhi@users.noreply.github.com>
2023-12-19 11:50:21 +0530

    Update provider.tf

--- module_example/provider.tf
+++ module_example/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXYR7AAF6CS7SNDUQ"
-  secret_key = "RL6VRd0oNwdnM83sDua9zarJPyMHpELAOOI7B6we"
+  access_key = "N/A"
+  secret_key = "N/A"
 
 
 }
